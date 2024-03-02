aa60533794c68337669305c3f4bf9e768f27e499
Paweł Sienkiewicz <sienkol2001@gmail.com>
2024-03-02 11:40:11 +0100

    Displaying random elements from MNIST database

--- digit_recognition.py
+++ digit_recognition.py
@@ -2,3 +2,15 @@ import numpy as np
 import struct
 from array import array
 from os.path import join
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+indexes = np.random.randint(0, len(x_test), 10)
+print(indexes)
+fig, axes = plt.subplots(1, 10, figsize=(25, 25))
+for i, index in enumerate(indexes):
+    axes[i].imshow(x_test[index], cmap="gray")
+    axes[i].set_title(f"Cyfra: {y_test[index]}")
+plt.show()
